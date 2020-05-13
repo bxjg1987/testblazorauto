b6555bf1ba48b473ca79117dbdd65f56ba530f89
利军 朱 <453008453@qq.com>
2020-05-13 18:53:23 +0800

    商品信息更新

--- WebAdmin/js/app.js
+++ WebAdmin/js/app.js
@@ -67,6 +67,13 @@
             });
         }, 0);
     };
+    abp.easyui.datagridBool = function (value, row, index) {
+
+        if (value)
+            return '<img style="" src="img/' + value + '.png" />';
+        return '';
+    };
+
 
     var inputWidth = 170;
     if ($.fn.textbox) {
