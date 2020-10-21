ccc14b15a32eab57c1970c97fb0ef6db58e46b1a
变形精怪 <453008453@qq.com>
2020-10-21 21:01:09 +0800

    商品分类图片功能基本可用

--- src/Shop/BXJG.Shop.Application/Catalogue/ProductCategoryAppService.cs
+++ src/Shop/BXJG.Shop.Application/Catalogue/ProductCategoryAppService.cs
@@ -1,9 +1,13 @@
 ﻿using Abp.Domain.Repositories;
 using BXJG.GeneralTree;
 using BXJG.Shop.Authorization;
+using BXJG.Utils.File;
+using NUglify.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BXJG.Shop.Catalogue
 {
@@ -18,13 +22,41 @@ namespace BXJG.Shop.Catalogue
                                                                            ProductCategoryEntity,
                                                                            ProductCategoryManager>, IProductCategoryAppService
     {
+        private readonly TempFileManager tempFileManager;
         public ProductCategoryAppService(IRepository<ProductCategoryEntity, long> ownRepository,
-                                              ProductCategoryManager organizationUnitManager) : base(ownRepository,
-                                                                                                  organizationUnitManager,
-                                                                                                  PermissionNames.ProductCategoryCreate,
-                                                                                                  PermissionNames.ProductCategoryUpdate,
-                                                                                                  PermissionNames.ProductCategoryDelete,
-                                                                                                  PermissionNames.ProductCategory)
-        { }
+                                         TempFileManager tempFileManager,
+                                         ProductCategoryManager organizationUnitManager) : base(ownRepository,
+                                                                                                organizationUnitManager,
+                                                                                                PermissionNames.ProductCategoryCreate,
+                                                                                                PermissionNames.ProductCategoryUpdate,
+                                                                                                PermissionNames.ProductCategoryDelete,
+                                                                                                PermissionNames.ProductCategory)
+        {
+            this.tempFileManager = tempFileManager;
+        }
+
+        public override async Task<ProductCategoryDto> CreateAsync(ProductCategoryEditDto input)
+        {
+            if (!input.Icon.IsNullOrWhiteSpace())
+                input.Icon = (await this.tempFileManager.MoveAsync(input.Icon)).Single().FileRelativePath;
+            if (!input.Image1.IsNullOrWhiteSpace())
+                input.Image1 = (await this.tempFileManager.MoveAsync(input.Image1)).Single().FileRelativePath;
+            if (!input.Image2.IsNullOrWhiteSpace())
+                input.Image2 = (await this.tempFileManager.MoveAsync(input.Image2)).Single().FileRelativePath;
+
+            return await base.CreateAsync(input);
+        }
+
+        public override async Task<ProductCategoryDto> UpdateAsync(ProductCategoryEditDto input)
+        {
+            if (!input.Icon.IsNullOrWhiteSpace())
+                input.Icon = (await this.tempFileManager.MoveAsync(input.Icon)).Single().FileRelativePath;
+            if (!input.Image1.IsNullOrWhiteSpace())
+                input.Image1 = (await this.tempFileManager.MoveAsync(input.Image1)).Single().FileRelativePath;
+            if (!input.Image2.IsNullOrWhiteSpace())
+                input.Image2 = (await this.tempFileManager.MoveAsync(input.Image2)).Single().FileRelativePath;
+
+            return await base.UpdateAsync(input);
+        }
     }
 }
