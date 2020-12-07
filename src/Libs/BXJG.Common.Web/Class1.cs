37e98dde660fbfb8889c37da6dcb0fa56ac0f0a0
利军 朱 <453008453@qq.com>
2020-12-07 18:16:28 +0800

    dfg

--- src/Libs/BXJG.Common.Web/Class1.cs
+++ src/Libs/BXJG.Common.Web/Class1.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using Microsoft.AspNetCore.Hosting;
 
 namespace BXJG.Common.Web
 {
     public class Class1
     {
+        public Class1(IWebHostEnvironment webEnvironment) { 
+        
+        }
     }
 }
