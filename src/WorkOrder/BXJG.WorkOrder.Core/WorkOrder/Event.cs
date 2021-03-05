ffae4b5eb727028c38783262427b54f8cd38ee79
变形精怪 <453008453@qq.com>
2021-03-05 17:52:30 +0800

    x

---
+++ src/WorkOrder/BXJG.WorkOrder.Core/WorkOrder/Event.cs
@@ -0,0 +1,38 @@
+﻿using Abp.Events.Bus;
+using Abp.Events.Bus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BXJG.WorkOrder.WorkOrder
+{
+    public class StatusChangedEventData : EntityEventData<OrderEntity>
+    {
+        public Status Original { get; private set; }
+        public StatusChangedEventData(OrderEntity entity, Status original) : base(entity)
+        {
+            Original = original;
+        }
+    }
+    //通知冗余字段引用方修改自己的数据
+    public class TitleChangedEventData : EntityEventData<OrderEntity>
+    {
+        public string Original { get; private set; }
+        public TitleChangedEventData(OrderEntity entity, string original) : base(entity)
+        {
+            Original = original;
+        }
+    }
+
+    public class UrgencyDegreeChangedEventData : EntityEventData<OrderEntity>
+    {
+        public UrgencyDegree Original { get; private set; }
+        public UrgencyDegreeChangedEventData(OrderEntity entity, UrgencyDegree original) : base(entity)
+        {
+            Original = original;
+        }
+    }
+    
+}
--- src/WorkOrder/BXJG.WorkOrder.Core/WorkOrder/OrderEntity.cs
+++ src/WorkOrder/BXJG.WorkOrder.Core/WorkOrder/OrderEntity.cs
@@ -41,9 +41,10 @@ namespace BXJG.WorkOrder.WorkOrder
                            string extendedField5 = default)
         {
             CategoryId = categoryId;
-            Status = Status.ToBeConfirm;
-            UrgencyDegree = urgencyDegree;
-            Title = title;
+            //部分赋值不要用属性，以免引起事件触发
+            this.status = Status.ToBeConfirm;
+            this.urgencyDegree = urgencyDegree;
+            this.title = title;
             Description = description;
             ChangeEstimatedTime(estimatedExecutionTime, estimatedCompletionTime);
             ExtendedField1 = extendedField1;
@@ -97,36 +98,74 @@ namespace BXJG.WorkOrder.WorkOrder
             {
                 if (Status == Status.Completed)
                 {
-                    throw new ApplicationException("已完成的工单不允许修改紧急程度！");
+                    throw new UserFriendlyException("已完成的工单不允许修改紧急程度！");
                 }
-                urgencyDegree = value;
+                if (value != urgencyDegree)
+                {
+                    var o = value;
+                    urgencyDegree = value;
+                    DomainEvents.Add(new UrgencyDegreeChangedEventData(this, o));
+                }
             }
         }
+        string title;
         /// <summary>
         /// 标题
         /// </summary>
-        public string Title { get; set; }
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException();
+
+                if (title != value)
+                {
+                    var o = title;
+                    title = value;
+                    //通知冗余字段更新
+                    DomainEvents.Add(new TitleChangedEventData(this, o));
+                }
+            }
+        }
         /// <summary>
         /// 内容描述
         /// </summary>
         public string Description { get; set; }
-        string completionDescription;
+        string statusDescription;
         /// <summary>
-        /// 完成情况说明
+        /// 当前状态情况说明
         /// </summary>
-        public string CompletionDescription
+        public string StatusDescription
         {
             get
             {
-                return completionDescription;
+                return statusDescription;
             }
             set
             {
-                if (Status != Status.Completed)
-                    throw new UserFriendlyException("未完成工单不允许设置完成情况！");
-                completionDescription = value;
+                //if (Status != Status.Completed)
+                //    throw new UserFriendlyException("未完成工单不允许设置完成情况！");
+                statusDescription = value;
             }
         }
+
+        //string rejectDescription;
+        ///// <summary>
+        ///// 拒绝说明
+        ///// </summary>
+        //public string RejectDescription
+        //{
+        //    get => rejectDescription;
+        //    set
+        //    {
+        //        if (Status != Status.Rejected)
+        //            throw new UserFriendlyException("已拒绝的工单才能设置【拒绝理由】");
+        //        rejectDescription = value;
+        //    }
+        //}
+
         /// <summary>
         /// 希望的开始时间
         /// </summary>
@@ -146,7 +185,8 @@ namespace BXJG.WorkOrder.WorkOrder
         public DateTimeOffset? CompletionTime { get; private set; }
 
         public string EmployeeId { get; private set; }
-        public string EmployeeName { get; private set; }
+        //冗余字段通过事件修改
+        public string EmployeeName { get; set; }
 
         public int TenantId { get; set; }
         public byte[] RowVersion { get; }
@@ -190,41 +230,86 @@ namespace BXJG.WorkOrder.WorkOrder
         /// </summary>
         /// <param name="employeeId"></param>
         /// <param name="employeeName"></param>
-        /// <param name="time"></param>
-        public void Distribute(string employeeId, string employeeName/*, DateTimeOffset? start = default, DateTimeOffset? end = default*/)
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        public void Distribute(string employeeId, string employeeName, DateTimeOffset? start = default, DateTimeOffset? end = default)
         {
             //各种判断
-            if ((int)Status.ToBeProcess > 1)
-                throw new UserFriendlyException("状态异常");
+            if ((int)Status > (int)Status.ToBeProcess)
+                throw new UserFriendlyException("当前状态不允许分配操作");
 
             EmployeeId = employeeId;
             EmployeeName = employeeName;
             Status = Status.ToBeProcess;
-            //ChangeEstimatedTime(start, end);
+            DateTimeOffset? s = EstimatedExecutionTime;
+            DateTimeOffset? e = EstimatedCompletionTime;
+            if (start.HasValue)
+                s = start;
+            if (end.HasValue)
+                e = end;
+            ChangeEstimatedTime(s, e);
         }
         /// <summary>
         /// 执行工单
         /// </summary>
         /// <param name="time"></param>
-        public void Execute(DateTimeOffset time)
+        public void Execute(DateTimeOffset time, bool checkStatus = true)
         {
-            //业务判断
-            if (Status != Status.ToBeProcess)
-                throw new UserFriendlyException("状态异常");
+            if (checkStatus)
+            {
+                //业务判断
+                if (Status != Status.ToBeProcess)
+                    throw new UserFriendlyException("状态异常！");
+            }
 
             //ExecutionTime = time;
             ChangePracticalTime(time, CompletionTime);
             Status = Status.Processing;
         }
         /// <summary>
-        /// 完成工单，也可用直接设置CompletionDescription修改说明
+        /// 完成工单
         /// </summary>
         /// <param name="time">完成时间</param>
-        /// <param name="desc">完成情况说明</param>
-        public void Completion(DateTimeOffset time, string desc)
+        /// <param name="desc">完成情况说明，也可用直接设置CompletionDescription属性修改说明</param>
+        /// <param name="checkStatus">是否检查工单当前状态，若不检查则任何状态的工单都可以立即设置为完成</param>
+        public void Completion(DateTimeOffset time, string desc = default, bool checkStatus = true)
         {
-            CompletionDescription = desc;//里面已经判断了状态
+            if (checkStatus && Status != Status.Processing)
+            {
+                throw new UserFriendlyException("只有处理中的工单才可以执行【完成】操作");
+            }
+
             ChangePracticalTime(ExecutionTime, time);
+            Status = Status.Completed;
+            StatusDescription = desc;
         }
+        /// <summary>
+        /// 拒绝<br />
+        /// 任何状态下的工单都可以直接拒绝，拒绝说明必须录入
+        /// </summary>
+        public void Reject(string desc)
+        {
+            Status = Status.Rejected;
+            StatusDescription = desc;
+        }
+
+        //public virtual OrderEntity Copy()
+        //{
+        //    return new OrderEntity();
+        //}
+
+        //这种情况直接拒绝复制一个就好了
+        ///// <summary>
+        ///// 重置<br />
+        ///// 重新回到初始创建状态
+        ///// </summary>
+        //public void Reset()
+        //{
+        //    completionDescription = default;
+        //    rejectDescription = default;
+        //    ChangeEstimatedTime(default, default);
+        //    ChangePracticalTime(default, default);
+        //    status = Status.ToBeConfirm;
+        //}
     }
 }
