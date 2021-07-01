b0f69f4186a2428c5ae67f51d5e7d8d062eff7f7
变形精怪 <453008453@qq.com>
2021-07-01 21:24:41 +0800

    登陆调整

--- src/ZLJ.Core/Authorization/Users/UserClaimsPrincipalFactory.cs
+++ src/ZLJ.Core/Authorization/Users/UserClaimsPrincipalFactory.cs
@@ -9,31 +9,38 @@ using System.Security.Claims;
 using System.Linq;
 using ZLJ.MultiTenancy;
 using Abp.Domain.Uow;
+using Microsoft.AspNetCore.Http;
 
 namespace ZLJ.Authorization.Users
 {
     public class UserClaimsPrincipalFactory : AbpUserClaimsPrincipalFactory<User, Role>
     {
+        private readonly IHttpContextAccessor contextAccessor;
         private readonly ICustomerLoginManager<User> customerLoginManager;
 
         public UserClaimsPrincipalFactory(
             UserManager userManager,
             RoleManager roleManager,
-            IOptions<IdentityOptions> optionsAccessor, ICustomerLoginManager<User> customerLoginManager,IUnitOfWorkManager unitOfWorkManager)
+            IOptions<IdentityOptions> optionsAccessor, ICustomerLoginManager<User> customerLoginManager, IUnitOfWorkManager unitOfWorkManager, IHttpContextAccessor contextAccessor)
             : base(
                   userManager,
                   roleManager,
-                  optionsAccessor,unitOfWorkManager)
+                  optionsAccessor, unitOfWorkManager)
         {
             this.customerLoginManager = customerLoginManager;
+            this.contextAccessor = contextAccessor;
         }
         public override async Task<ClaimsPrincipal> CreateAsync(User user)
         {
             var claim = await base.CreateAsync(user);
-            var c = await customerLoginManager.GetBusinessUserClaim(user);
-            if(c!=null)
-            claim.Identities.First().AddClaim(c);
-
+            if (contextAccessor.HttpContext.Request.Query.TryGetValue("userType", out var longUserType))
+            {
+                if (longUserType.First().Equals(customerLoginManager.claimKey, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    var c = await customerLoginManager.GetBusinessUserClaim(user);
+                    claim.Identities.First().AddClaim(c);
+                }
+            }
             return claim;
         }
     }
