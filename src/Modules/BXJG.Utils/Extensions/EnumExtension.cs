6e32c73779f15177370a607ef9d539e152f750e4
bxjg <453008453@qq.com>
2022-05-15 11:30:15 +0800

    bb

--- src/Modules/BXJG.Utils/Extensions/EnumExtension.cs
+++ src/Modules/BXJG.Utils/Extensions/EnumExtension.cs
@@ -27,5 +27,23 @@ namespace BXJG.Utils.Extensions
             }
             return name;
         }
+
+        /// <summary>
+        /// 获取枚举的颜色描述信息
+        /// </summary>
+        public static string GetColor(this Enum em)
+        {
+            Type type = em.GetType();
+            FieldInfo fd = type.GetField(em.ToString());
+            if (fd == null)
+                return string.Empty;
+            object[] attrs = fd.GetCustomAttributes(typeof(ColorAttribute), false);
+            string name = string.Empty;
+            foreach (ColorAttribute attr in attrs)
+            {
+                name = attr.ColorHexStr;
+            }
+            return name;
+        }
     }
 }
