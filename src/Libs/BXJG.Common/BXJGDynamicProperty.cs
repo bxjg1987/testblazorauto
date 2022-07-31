7f3cccdea6d9e653a57cd56c04429bf7ca90c3fa
bxjg <453008453@qq.com>
2022-07-31 18:06:49 +0800

    dg

--- src/Libs/BXJG.Common/BXJGDynamicProperty.cs
+++ src/Libs/BXJG.Common/BXJGDynamicProperty.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BXJG.Common.Extensions;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -259,13 +260,22 @@ namespace BXJG.Common
         /// 差值
         /// </summary>
         public TValue DifferenceValue { get; set; }
-
+        public Func<TValue, TValue, bool> Comparer { get; set; }=(a, b) =>
+        {
+            if (a is IEnumerable)
+            {
+                var a1 = a as IEnumerable<object>;
+                var b1 = b as IEnumerable<object>;
+                return a1.HasChange(b1);
+            }
+            return !a.Equals(b);
+        };
         //这个问题太复杂，如果让调用方设置，太繁琐
         //如果自己来设置，简单的Equals无法满足需求，比如两个null值，返回true
         /// <summary>
         /// 是否有变动
         /// </summary>
-        public bool IsChanged => !this.OriginalValue.Equals(this.CurrentValue);
+        public bool IsChanged => Comparer(OriginalValue, CurrentValue);
     }
     /// <summary>
     /// 后台管理合同属性变更显示模型
---
+++ src/Libs/BXJG.Common/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BXJG.Common.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// 判断两个集合是否有变化
+        /// 项使用Eq
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool HasChange<T>(this IEnumerable<T> a, IEnumerable<T> b)
+        {
+            if (a == null && b != null)
+                return true;
+
+            if (a != null && b == null)
+                return true;
+
+            if (a.Count() != b.Count())
+                return true;
+
+            return a.Any(c => b.Any(d => !d.Equals(c)));
+        }
+
+        //public static Func<IEnumerable<T>, IEnumerable<T>, bool> CreateComparer<T>()
+        //{
+        //    return HasChange;
+        //}
+    }
+}
