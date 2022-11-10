b419ffd6fff36b003c922c70c9134ad92703c2d1
bxjg <453008453@qq.com>
2022-11-11 00:04:19 +0800

    基本的blazorECharts组件实现

---
+++ src/Libs/BXJG.BlazorECharts/BXJGBlazorECharts.razor.js
@@ -0,0 +1,14 @@
+﻿export function init(container, option) {
+    let thisECharts = echarts.init(container);
+    if (option) {
+        thisECharts.setOption(option);
+    }
+}
+
+export function setOption(container, option) {
+    echarts.getInstanceByDom(container).setOption(option);
+}
+
+export function dispose(container) {
+    echarts.dispose(container);
+}
