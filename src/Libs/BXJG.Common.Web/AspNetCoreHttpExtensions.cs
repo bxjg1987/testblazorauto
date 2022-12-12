2f6412874406da03786adc48a6b252173d8d47d4
bxjg <453008453@qq.com>
2022-12-12 20:30:49 +0800

    集成blazor基本完成，支持多端app分离

---
+++ src/Libs/BXJG.Common.Web/AspNetCoreHttpExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Http
+{
+    public static class AspNetCoreHttpExtensions
+    {
+        /// <summary>
+        /// abp那个判断对新浏览器没用
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static bool IsAjaxRequestBXJG(this HttpRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            if (request.Headers != null)
+            {
+                if (request.Headers.TryGetValue("Sec-Fetch-Mode", out var cors))
+                {
+                    if (cors == "cors")
+                        return true;
+                }
+
+              return request.Headers["X-Requested-With"] == "XMLHttpRequest";//|| request.Headers.ContainsKey("Sec-Fetch-Dest");
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Modules/BXJG.Utils.RCL/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BXJG.Utils.RCL.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
