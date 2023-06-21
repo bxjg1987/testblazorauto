4e719f62763d33673c452056b15e17571ad507b5
变形精怪 <453008453@qq.com>
2023-06-21 03:17:41 +0000

    zhongjie注册时，返回IDispose
    Signed-off-by: 变形精怪 <453008453@qq.com>

--- src/Libs/BXJG.Common/DongtaiZhongjie/Zhongjie.cs
+++ src/Libs/BXJG.Common/DongtaiZhongjie/Zhongjie.cs
@@ -11,6 +11,8 @@ namespace BXJG.Common.DongtaiZhongjie
 {
 
     /*
+     * 注意：abp文档事件底部已经说明了可以动态注册，所以在abp项目中不需要使用Zhongjie类，直接用abp的就好
+     *
      * 触发事件时，触发方并不晓得事件处理方需要同步还是异步，定义事件时也无法确定，因为事件处理程序是动态注册的。
      * 也无法等待返回，因为可能存在多个处理方
      * 通用的办法是假定所有处理方都是异步的，使用ValueTask，触发时使用Task.WhenAll
@@ -51,7 +53,7 @@ namespace BXJG.Common.DongtaiZhongjie
             logger.LogDebug($"事件类型数量：{this.Count}，总委托数：{k}");
         }
 
-        public virtual void Zhuce<T>(Func<T, ValueTask> weituo, string eventName = default) where T : class
+        public virtual IDispose Zhuce<T>(Func<T, ValueTask> weituo, string eventName = default) where T : class
         {
             if (eventName.IsNullOrWhiteSpaceBXJG())
                 eventName = typeof(T).FullName;
@@ -66,8 +68,9 @@ namespace BXJG.Common.DongtaiZhongjie
 
             LogDebug();
             // TryAdd(typeof(T), oo => weituo(oo as T));
+            return new ZhongjieZhuxiaoqi(this,weituo,eventName);
         }
-        public virtual void Zhuce(Func<ValueTask> weituo, string eventName)
+        public virtual IDispose Zhuce(Func<ValueTask> weituo, string eventName)
         {
             TryAdd(eventName, new ConcurrentDictionary<Delegate, Func<object, ValueTask>>());
 
@@ -76,6 +79,7 @@ namespace BXJG.Common.DongtaiZhongjie
             logger.LogDebug($"注册事件：{eventName}");
 
             LogDebug();
+            return new ZhongjieZhuxiaoqi(this,weituo,eventName);
             // TryAdd(typeof(T), oo => weituo(oo as T));
         }
         //不用Delegate做参数类型，因为外面调用时，自动推导调用起来更容易
@@ -129,87 +133,24 @@ namespace BXJG.Common.DongtaiZhongjie
                 await Task.WhenAll(dic.Select(c => c.Value(null).AsTask()));
             }
         }
+
+        internal class ZhongjieZhuxiaoqi : IDispose
+        {
+            private readonly Zhongjie zhongjie;
+            private readonly string eventName;
+            private readonly Delegate act;
+
+            public ZhongjieZhuxiaoqi(Zhongjie zj,string eventName, Delegate act)
+            {
+                this.zhongjie=zj;
+                this.eventName=eventName;
+                this.act=act;
+            }
+
+            public void Dispose()
+            {
+                this.zhongjie.Zhuxiao(act,eventName);
+            }
+        }
     }
-
-    ///// <summary>
-    ///// 有参事件中介 
-    ///// Type事件（参数）类型
-    ///// 委托 要执行的委托，key是原始委托，value是转换后的委托
-    ///// </summary>
-    //public class Zhongjie : ConcurrentDictionary<Type, ConcurrentDictionary<Delegate, Func<object, ValueTask>>>
-    //{
-    //    //
-
-    //    // private readonly ConcurrentBag<Func< object, Func<object, ValueTask>>> tongbuChuliqi = new ConcurrentBag<Func< object, Func<object, ValueTask>>>();
-
-
-    //    //public void Zhuce(Action act) 
-    //    //{ 
-
-    //    //}
-
-    //    //public void Zhuxiao() { 
-
-    //    //}
-
-    //    public virtual void Zhuce<T>(Func<T, ValueTask> weituo) where T : class
-    //    {
-    //        var t = typeof(T);
-
-    //        TryAdd(t, new ConcurrentDictionary<Delegate, Func<object, ValueTask>>());
-
-    //        this[t].TryAdd(weituo, oo => weituo(oo as T));
-
-    //        // TryAdd(typeof(T), oo => weituo(oo as T));
-    //    }
-
-    //    public virtual void Zhuxiao<T>(Func<T, ValueTask> weituo)
-    //    {
-    //        var t = typeof(T);
-    //        if (TryGetValue(t, out var dic))
-    //        {
-    //            dic.TryRemove(weituo, out _);
-    //        }
-
-
-    //        // TryRemove(weituo.GetHashCode(), out _);
-    //    }
-
-    //    public virtual async ValueTask Chufa(object canshu)
-    //    {
-    //        var t = canshu.GetType();
-    //        if (TryGetValue(t, out var dic))
-    //        {
-    //            await Task.WhenAll(dic.Select(c => c.Value(canshu).AsTask()));
-    //        }
-    //        // await Task.WhenAll(this.Select(c => c.Value(canshu).AsTask()));
-    //    }
-    //}
-    ///// <summary>
-    ///// 无参事件中介
-    ///// </summary>
-    //public class ZhongjieWithoutParam : ConcurrentDictionary<string, HashSet<Func<ValueTask>>>
-    //{
-    //    public virtual void Zhuce(string e, Func<ValueTask> func)
-    //    {
-    //        this.TryAdd(e, new HashSet<Func<ValueTask>>());
-    //        this[e].Add(func);
-    //    }
-
-    //    public virtual void Zhuxiao(Func<ValueTask> func)
-    //    {
-    //        foreach (var item in this)
-    //        {
-    //            item.Value.Remove(func);
-    //        }
-    //    }
-
-    //    public virtual async ValueTask Chufa(string shijian)
-    //    {
-    //        if (TryGetValue(shijian, out var dic))
-    //        {
-    //            await Task.WhenAll(dic.Select(c => c().AsTask()));
-    //        }
-    //    }
-    //}
 }
