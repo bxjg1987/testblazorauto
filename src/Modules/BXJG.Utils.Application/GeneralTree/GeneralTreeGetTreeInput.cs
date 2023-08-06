775e19b2d91bb471136c0ec604f555670949d281
bxjg <453008453@qq.com>
2023-08-06 23:46:06 +0800

    小调整

--- src/Modules/BXJG.Utils.Application/GeneralTree/GeneralTreeGetTreeInput.cs
+++ src/Modules/BXJG.Utils.Application/GeneralTree/GeneralTreeGetTreeInput.cs
@@ -14,7 +14,7 @@ namespace BXJG.Utils.GeneralTree
         //不要使用Nullable<TId>类型，因为某些情况此参数可能不允许为null
         //若数据很多 可能需要么次加载一个节点的数据，所以ParentId字段是必要的
 
-        public string ParentCode { get; set; }
+        public string? ParentCode { get; set; }
         /// <summary>
         /// 父级节点id，非必填
         /// </summary>
@@ -26,7 +26,13 @@ namespace BXJG.Utils.GeneralTree
         /// <summary>
         /// 若LoadParent为true且ParentId为空，则自动以{id:0,text:L( ParentText) }创建一个虚拟父节点，注意L是处理本地化的方法，因此ParentText值的是本地化的key
         /// </summary>
-        public string ParentText { get; set; }
+        public string? ParentText { get; set; }
+
+        //由于关键字是or查询，若要让子类和父类的关键字条件在一个or链中，会让父类变得复杂，因此决定不在父类中定义关键字
+        ///// <summary>
+        ///// 关键字，若为空则限制
+        ///// </summary>
+        //public string? Keywords { get; set; }
 
         //没必要在这里定义初始值，因为需要处理本地化
         //若想在这里处理需要引入LocationManager，但是这个类是被模型绑定实例化的，因此比较麻烦，不确定abp的模型绑定是否注入本地化相关类，所以不再这个类的内部来做本地化
