96acc01f308850d7e13081f87302247ee1418e9a
bxjg <453008453@qq.com>
2023-09-09 22:47:14 +0800

    xxxx

---
+++ src/Modules/BXJG.MudBlazor/Components/AbpMudBlazorFormBaseComponent.cs
@@ -0,0 +1,91 @@
+﻿using Abp.Application.Services.Dto;
+using BXJG.Common;
+using BXJG.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BXJG.MudBlazor.Components
+{
+    /// <summary>
+    /// 新增和修改的表单抽象组件
+    /// </summary>
+    /// <typeparam name="TAppService">应用服务类型</typeparam>
+    /// <typeparam name="TEntityDto">列表项的数据类型</typeparam>
+    /// <typeparam name="TPrimaryKey">唯一id类型</typeparam>
+    /// <typeparam name="TGetAllInput">获取列表时的输入参数类型</typeparam>
+    /// <typeparam name="TCreateInput">新增时的输入类型</typeparam>
+    /// <typeparam name="TUpdateInput">修改时的输入类型</typeparam>
+    public class AbpMudBlazorFormBaseComponent<TAppService,
+                                               TEntityDto,
+                                               TPrimaryKey,
+                                               TGetAllInput,
+                                               TCreateInput,
+                                               TUpdateInput> : AbpMudBlazorBaseComponent
+        where TEntityDto : IEntityDto<TPrimaryKey>
+        where TGetAllInput : new()
+        where TUpdateInput : IEntityDto<TPrimaryKey>
+        where TAppService : ICrudBaseAppService<TEntityDto, TPrimaryKey, TGetAllInput, TCreateInput, TUpdateInput>
+    {
+        /// <summary>
+        /// 缓存当前主服务对象
+        /// </summary>
+        private TAppService? appService;
+        /// <summary>
+        /// 获取主服务
+        /// </summary>
+        protected virtual TAppService AppService => appService ??= ScopedServices.GetRequiredService<TAppService>();
+        /// <summary>
+        /// 此功能的名称
+        /// </summary>
+        protected virtual string FuncName => $"请重写{nameof(FuncName)}属性";
+        /// <summary>
+        /// 表单模式
+        /// </summary>
+        public virtual FrmPattern Pattern { get; set; }
+        /// <summary>
+        /// 编辑时的模型
+        /// </summary>
+        public virtual TEntityDto Model { get; set; }
+
+        protected override async Task OnInitialized2Async()
+        {
+            //列表传递过来的dto信息没有详情中的dto多
+            Model = await AppService.GetAsync(new EntityDto<TPrimaryKey>(Model.Id));
+        }
+
+        #region 权限
+        /// <summary>
+        /// 是否有新增权限
+        /// </summary>
+        protected bool createIsGranted = true;
+        /// <summary>
+        /// 是否有修改权限
+        /// </summary>
+        protected bool updateIsGranted = true;
+        /// <summary>
+        /// 是否有删除权限
+        /// </summary>
+        protected bool deleteIsGranted = true;
+        /// <summary>
+        /// 初始化权限状态
+        /// </summary>
+        /// <param name="createPermissionName"></param>
+        /// <param name="updatePermissionName"></param>
+        /// <param name="deletePermissionName"></param>
+        /// <returns></returns>
+        protected virtual async Task InitPermission(string createPermissionName = default, string updatePermissionName = default, string deletePermissionName = default)
+        {
+            if (createPermissionName.IsNotNullOrWhiteSpaceBXJG())
+                createIsGranted = await PermissionChecker.IsGrantedAsync(createPermissionName);
+            if (updatePermissionName.IsNotNullOrWhiteSpaceBXJG())
+                updateIsGranted = await PermissionChecker.IsGrantedAsync(updatePermissionName);
+            if (deletePermissionName.IsNotNullOrWhiteSpaceBXJG())
+                deleteIsGranted = await PermissionChecker.IsGrantedAsync(deletePermissionName);
+        }
+        #endregion
+    }
+}
