0f11f49e8261568951c4515ef2add6a5cb739558
利军 朱 <453008453@qq.com>
2023-12-06 22:29:42 +0800

    渐进式blazor，去掉多标签布局，布局改为静态，菜单改为auto，因为需要交互，各页面自己设置交互模式。

--- src/ZLJ.Admin.CoreRCL/Program.cs
+++ src/ZLJ.Admin.CoreRCL/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
+builder.Services.AddAntDesign();
 await builder.Build().RunAsync();
