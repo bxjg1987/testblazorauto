347d56a7c9b8fc811ecf737afd1c2d98f9e7985c
Zhu Lijun <453008453@qq.com>
2024-03-17 23:25:04 +0800

    x

--- src/ZLJ.Web.HostBlazor/Components/App.razor.cs
+++ src/ZLJ.Web.HostBlazor/Components/App.razor.cs
@@ -56,14 +56,12 @@ namespace ZLJ.Web.HostBlazor.Components
              */
             if (xs == false)
             {
-                //var assemblyName = typeof(ZLJ.Admin.CoreRCL.Share.Routes).Assembly.GetName().Name + ".dll";
-                //var dir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-                //var hash = Path.Combine(dir, assemblyName).GetMD5ByFilePath();
+                var assemblyName = typeof(ZLJ.Admin.CoreRCL.Share.Routes).Assembly.GetName().Name + ".dll";
+                var dir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                var hash = Path.Combine(dir, assemblyName).GetMD5ByFilePath();
                 // Console.WriteLine(Environment.CurrentDirectory + "===" + hash);
                 
-                //部分dll更新时，blazor只会自动下载更新那部分dll，所以不用与dll的hash关联，而且这里之关联一个dll也不方便，也该是所有dll的hash之后再hash下
-                //所以干脆不管了
-                var cookieKey = prefix;// + hash; 
+                var cookieKey = prefix + hash; 
                 if (HttpContext.Request.Cookies.TryGetValue(cookieKey, out var sj))
                 {
                     //在网络好时，15秒左右下载完成，网络不好时设置久点，大不了首次请求时服务器多支撑一会
@@ -81,7 +79,7 @@ namespace ZLJ.Web.HostBlazor.Components
                         if (item.StartsWith(prefix))
                             HttpContext.Response.Cookies.Delete(item);
                     }
-                    HttpContext.Response.Cookies.Append(cookieKey, DateTime.Now.ToString(),new CookieOptions { Expires= DateTimeOffset.Now.AddYears(10) });
+                    HttpContext.Response.Cookies.Append(cookieKey, DateTime.Now.ToString(),new CookieOptions { Expires= DateTimeOffset.Now.AddYears(5) });
                 }
             }
 
