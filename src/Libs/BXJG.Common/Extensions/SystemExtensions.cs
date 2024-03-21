0f7180fa045162d409ed3813c6133d115bb645a3
Zhu Lijun <453008453@qq.com>
2024-03-21 22:03:06 +0800

    更新nuget

--- src/Libs/BXJG.Common/Extensions/SystemExtensions.cs
+++ src/Libs/BXJG.Common/Extensions/SystemExtensions.cs
@@ -23,58 +23,64 @@ namespace System
 {
     public static class SystemExtensions
     {
-        private const int Second = 1;
-        private const int Minute = 60 * Second;
-        private const int Hour = 60 * Minute;
-        private const int Day = 24 * Hour;
-        private const int Month = 30 * Day;
-        public static string ToFriendlyDisplay(this DateTime dateTime)
-        {
-            var ts = DateTime.Now - dateTime;
-            var delta = ts.TotalSeconds;
-            if (delta < 0)
-            {
-                return "not yet";
-            }
-            if (delta < 1 * Minute)
-            {
-                return ts.Seconds == 1 ? "1 second ago" : ts.Seconds + " seconds ago";
-            }
-            if (delta < 2 * Minute)
-            {
-                return "1 minute ago";
-            }
-            if (delta < 45 * Minute)
-            {
-                return ts.Minutes + "minute";
-            }
-            if (delta < 90 * Minute)
-            {
-                return "1 hour ago";
-            }
-            if (delta < 24 * Hour)
-            {
-                return ts.Hours + " hours ago";
-            }
-            if (delta < 48 * Hour)
-            {
-                return "yesterday";
-            }
-            if (delta < 30 * Day)
-            {
-                return ts.Days + " days ago";
-            }
-            if (delta < 12 * Month)
-            {
-                var months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
-                return months <= 1 ? "A month ago" : months + " months ago";
-            }
-            else
-            {
-                var years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
-                return years <= 1 ? "a year ago" : years + " years ago";
-            }
-        }
+//        private const int Second = 1;
+//        private const int Minute = 60 * Second;
+//        private const int Hour = 60 * Minute;
+//        private const int Day = 24 * Hour;
+//        private const int Month = 30 * Day;
+//        public static string ToFriendlyDisplay(this DateTime dateTime)
+//        {
+//#if NET8_0_OR_GREATER
+//            var ts = TimeProvider.System.GetLocalNow().Date - dateTime;
+//#else
+//            var ts = DateTime.Now - dateTime;
+//#endif
+
+
+//            var delta = ts.TotalSeconds;
+//            if (delta < 0)
+//            {
+//                return "现在";
+//            }
+//            if (delta < 1 * Minute)
+//            {
+//                return ts.Seconds == 1 ? "1秒钟前" : ts.Seconds + "秒之前";
+//            }
+//            if (delta < 2 * Minute)
+//            {
+//                return "1分钟之前";
+//            }
+//            if (delta < 45 * Minute)
+//            {
+//                return ts.Minutes + "分钟";
+//            }
+//            if (delta < 90 * Minute)
+//            {
+//                return "1小时前";
+//            }
+//            if (delta < 24 * Hour)
+//            {
+//                return ts.Hours + "小时前";
+//            }
+//            if (delta < 48 * Hour)
+//            {
+//                return "昨天";
+//            }
+//            if (delta < 30 * Day)
+//            {
+//                return ts.Days + "天前";
+//            }
+//            if (delta < 12 * Month)
+//            {
+//                var months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
+//                return months <= 1 ? "个月之前" : months + "个月之前";
+//            }
+//            else
+//            {
+//                var years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
+//                return years <= 1 ? "年之前" : years + "年之前";
+//            }
+//        }
         /// <summary>
         /// 获取类型默认值
         /// </summary>
