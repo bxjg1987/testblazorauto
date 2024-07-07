2945cfc68d26e73c6cdef2cad0a14fb958d61d3a
Zhu Lijun <453008453@qq.com>
2024-07-07 23:55:53 +0800

    肉夹馍3问题彻底解决

--- src/ZLJ.Web.BlazorAuto/Components/App.razor.cs
+++ src/ZLJ.Web.BlazorAuto/Components/App.razor.cs
@@ -63,8 +63,8 @@ namespace ZLJ.Web.BlazorAuto.Components
                 if (HttpContext.Request.Path.StartsWithSegments("/error"))
                     return default;
 
-                // return new InteractiveAutoRenderMode(false);
-               return new InteractiveServerRenderMode(false);
+               return new InteractiveAutoRenderMode(false);
+            // return new InteractiveServerRenderMode(false);
                 // return new InteractiveWebAssemblyRenderMode(false);
             }
         }
