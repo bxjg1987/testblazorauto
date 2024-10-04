839586ff30804c26f16c48adcb7382cff12a28dc
Think <Think@DESKTOP-OT5OQTG>
2024-10-05 01:32:33 +0800

    x

---
+++ src/Modules/BXJG.Utils/Events/EntityUpdatingEventDataBXJG.cs
@@ -0,0 +1,28 @@
+﻿using Abp.Events.Bus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BXJG.Utils.Events
+{
+    public class EntityUpdatingEventDataBXJG<TEntity> : EntityUpdatingEventData<TEntity>
+    {
+        public object OriginalValue { get; protected set; }   
+        public EntityUpdatingEventDataBXJG(TEntity entity,object obj) : base(entity)
+        {
+            this.OriginalValue = obj;
+        }
+    }
+    public class EntityUpdatingEventDataBXJG<TEntity,TState> : EntityUpdatingEventDataBXJG<TEntity>
+    {
+        public EntityUpdatingEventDataBXJG(TEntity entity, object obj) : base(entity, obj)
+        {
+            this.OriginalValue = (TState) Convert.ChangeType(obj, typeof(TState));
+        }
+
+        public new TState OriginalValue { get; protected set; }
+       
+    }
+}
---
+++ src/Modules/BXJG.Utils/Extensions/RootExt.cs
@@ -0,0 +1,11 @@
+using Abp.Domain.Entities;
+using System;
+using System.Linq.Expressions;
+
+public static class sdfsfsd
+{
+    public static void sdfsfdsdf<TEntity, TState,TEventData>(this TEntity entity,Expression<Func<TEntity, TState>> hq) where TEntity : IGeneratesDomainEvents
+    {
+
+    }
+}
