c085dbbd68cd746e4e68a7a1a749574b4b1c1d29
Think <Think@DESKTOP-OT5OQTG>
2024-10-31 00:25:03 +0800

    x

--- src/Modules/BXJG.Utils.Web/AppBuilderExtensions.cs
+++ src/Modules/BXJG.Utils.Web/AppBuilderExtensions.cs
@@ -17,7 +17,7 @@ namespace BXJG.Utils.Web
     {
         public static IApplicationBuilder UseStaticAbpDI(this IApplicationBuilder appBuilder)
         {
-            return appBuilder.UseMiddleware<AbpDIMiddleware>();  
+            return appBuilder.UseMiddleware<AbpDIMiddleware>();
         }
     }
 
@@ -34,11 +34,23 @@ namespace BXJG.Utils.Web
         // 这里只是个兜底，应用层使用了拦截器再次设置
         public async Task InvokeAsync(HttpContext httpContext)
         {
-            AbpDIStaticAccessor._resolver.Value = httpContext.RequestServices.GetRequiredService<IScopedIocResolver>();
-            //Console.WriteLine("");
-            //httpContext.RequestServices.GetRequiredService<ILogger>().Debug($"请求中间件查看当前IocResolver：{AbpDIStaticAccessor._resolver.Value.GetHashCode()}");
-            await _next(httpContext);
-            AbpDIStaticAccessor._resolver.Value = default;//asynclocal基于线程，请求线程结束了，这里有点多余 好像。
+            //AbpKernelModule IocManager.Register<IScopedIocResolver, ScopedIocResolver>(DependencyLifeStyle.Transient);
+            //所以是安全的
+            var old = AbpDIStaticAccessor._resolver.Value;
+            using (var temp = httpContext.RequestServices.GetRequiredService<IScopedIocResolver>())
+            {
+                AbpDIStaticAccessor._resolver.Value = temp;
+                //Console.WriteLine("");
+                //httpContext.RequestServices.GetRequiredService<ILogger>().Debug($"请求中间件查看当前IocResolver：{AbpDIStaticAccessor._resolver.Value.GetHashCode()}");
+                try
+                {
+                    await _next(httpContext);
+                }
+                finally
+                {
+                    AbpDIStaticAccessor._resolver.Value = old;//asynclocal基于线程，请求线程结束了，这里有点多余 好像。
+                }
+            }
         }
     }
 }
--- src/Modules/BXJG.Utils/DI/StaticDIAccessInterceptor.cs
+++ src/Modules/BXJG.Utils/DI/StaticDIAccessInterceptor.cs
@@ -10,39 +10,67 @@ namespace BXJG.Utils.DI
 {
     public class StaticDIAccessInterceptor : AbpInterceptorBase, ITransientDependency
     {
+        //AbpKernelModule IocManager.Register<IScopedIocResolver, ScopedIocResolver>(DependencyLifeStyle.Transient);
+
         public IScopedIocResolver ScopedIocResolver { get; set; }
         public Castle.Core.Logging.ILogger Logger { get; set; }
-       
+
         public override void InterceptSynchronous(IInvocation invocation)
         {
             var old = AbpDIStaticAccessor._resolver.Value;
             AbpDIStaticAccessor._resolver.Value = ScopedIocResolver;
-            //Logger.Debug($"abp拦截器查看当前IocResolver：{AbpDIStaticAccessor._resolver.Value.GetHashCode()}");
-            invocation.Proceed();
-            AbpDIStaticAccessor._resolver.Value = old;
+            using (ScopedIocResolver)
+            {
+                //Logger.Debug($"abp拦截器查看当前IocResolver：{AbpDIStaticAccessor._resolver.Value.GetHashCode()}");
+                try
+                {
+                    invocation.Proceed();
+                }
+                finally
+                {
+                    AbpDIStaticAccessor._resolver.Value = old;
+                }
+            }
         }
 
         protected override async Task InternalInterceptAsynchronous(IInvocation invocation)
         {
-            var proceedInfo = invocation.CaptureProceedInfo();
             var old = AbpDIStaticAccessor._resolver.Value;
             AbpDIStaticAccessor._resolver.Value = ScopedIocResolver;
-            proceedInfo.Invoke();
-            //Logger.Debug($"abp拦截器查看当前IocResolver：{AbpDIStaticAccessor._resolver.Value?.GetHashCode()}");
-            await (Task)invocation.ReturnValue;
-            AbpDIStaticAccessor._resolver.Value = old;
+            using (ScopedIocResolver)
+            {
+                try
+                {
+                    var proceedInfo = invocation.CaptureProceedInfo();
+                    proceedInfo.Invoke();
+                    //Logger.Debug($"abp拦截器查看当前IocResolver：{AbpDIStaticAccessor._resolver.Value?.GetHashCode()}");
+                    await (Task)invocation.ReturnValue;
+                }
+                finally
+                {
+                    AbpDIStaticAccessor._resolver.Value = old;
+                }
+            }
         }
 
         protected override async Task<TResult> InternalInterceptAsynchronous<TResult>(IInvocation invocation)
         {
-            var proceedInfo = invocation.CaptureProceedInfo();
             var old = AbpDIStaticAccessor._resolver.Value;
             AbpDIStaticAccessor._resolver.Value = ScopedIocResolver;
-            proceedInfo.Invoke();
-            //Logger.Debug($"abp拦截器查看当前IocResolver：{AbpDIStaticAccessor._resolver.Value?.GetHashCode()}");
-            var r = await (Task<TResult>)invocation.ReturnValue;
-            AbpDIStaticAccessor._resolver.Value = old;
-            return r;
+            using (ScopedIocResolver)
+            {
+                try
+                {
+                    var proceedInfo = invocation.CaptureProceedInfo();
+                    proceedInfo.Invoke();
+                    //Logger.Debug($"abp拦截器查看当前IocResolver：{AbpDIStaticAccessor._resolver.Value?.GetHashCode()}");
+                    return await (Task<TResult>)invocation.ReturnValue;
+                }
+                finally
+                {
+                    AbpDIStaticAccessor._resolver.Value = old;
+                }
+            }
         }
     }
 }
