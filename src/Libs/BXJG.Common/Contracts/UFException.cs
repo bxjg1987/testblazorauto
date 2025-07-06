87befffde07aacafd654ee15bea2daed4a3db763
Think <Think@DESKTOP-OT5OQTG>
2025-07-07 01:05:45 +0800

    z

---
+++ src/Libs/BXJG.Common/Contracts/UFException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BXJG.Common.Contracts
+{
+    /// <summary>
+    /// 用户友好的移除
+    /// </summary>
+    public class UFException : Exception
+    {
+        public UFException()
+        {
+        }
+
+        public UFException(string message) : base(message)
+        {
+        }
+
+        public UFException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UFException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
