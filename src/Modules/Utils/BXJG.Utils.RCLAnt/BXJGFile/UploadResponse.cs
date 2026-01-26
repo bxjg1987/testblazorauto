f23960a494d612f1c3be952b17160b61b2509b19
Think <Think@DESKTOP-OT5OQTG>
2026-01-27 06:15:52 +0800

    封装独立的新增数据时的上传组件

---
+++ src/Modules/Utils/BXJG.Utils.RCLAnt/BXJGFile/UploadResponse.cs
@@ -0,0 +1,10 @@
+using BXJG.Utils.Share.Files;
+
+namespace BXJG.Utils.RCLAnt.BXJGFile
+{
+    public class UploadResponse
+    {
+        public bool Success { get; set; }
+        public SetAttachmentFile? Result { get; set; }
+    }
+}
