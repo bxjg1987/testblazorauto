dd95fba639ad5263ddab619bd024e5bbc1911a64
Think <Think@DESKTOP-OT5OQTG>
2026-02-04 15:19:14 +0800

    uniapp继续；自动混淆（未完成）

---
+++ src/Modules/Utils/BXJG.Utils.Share/Menu/MenuShowModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BXJG.Utils.Share.Menu
+{
+    /// <summary>
+    /// 菜单显示模式
+    /// </summary>
+    [Flags]
+    public enum MenuShowModel
+    {
+        /// <summary>
+        /// 不显示
+        /// </summary>
+        [Display(Name ="不显示")]
+        None = 1<< 0,
+        /// <summary>
+        /// 主菜单
+        /// </summary>
+        [Display(Name = "主菜单")]
+        Main = 1 << 1,
+        /// <summary>
+        /// 普通
+        /// </summary>
+        [Display(Name = "普通")]
+        Normal = 1 << 2,
+    }
+}
