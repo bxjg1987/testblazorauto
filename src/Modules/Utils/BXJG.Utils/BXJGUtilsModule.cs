0dc8f19132c935a6f0c8b18bb220e27f814bebee
Think <Think@DESKTOP-OT5OQTG>
2026-05-22 15:32:53 +0800

    回复前端auto处理；setting调整；列表自适应高度

--- src/Modules/Utils/BXJG.Utils/BXJGUtilsModule.cs
+++ src/Modules/Utils/BXJG.Utils/BXJGUtilsModule.cs
@@ -1,10 +1,12 @@
-﻿using Abp;
+using Abp;
 using Abp.AutoMapper;
+using Abp.Configuration;
 using Abp.Configuration.Startup;
 using Abp.Dependency;
 using Abp.Domain.Entities;
 using Abp.Domain.Uow;
 using Abp.Modules;
+using Abp.Net.Mail;
 using Abp.Organizations;
 using Abp.RealTime;
 using Abp.Reflection.Extensions;
@@ -12,6 +14,7 @@ using Abp.Runtime.Session;
 using Abp.Threading;
 using Abp.Threading.BackgroundWorkers;
 using Abp.Timing;
+using Abp.Zero.Configuration;
 using AutoMapper;
 using BXJG.Common;
 using BXJG.Common.Contracts;
@@ -179,20 +182,134 @@ namespace BXJG.Utils
             base.IocManager.RegService(c => c.TryAddSingleton(sdf));
             #endregion
 
+            AdjustBuiltInSettingDefinitions();
+        }
 
+        /// <summary>
+        /// 调整ABP内置设置项的分组和显示名。
+        /// 用户管理设置项的DisplayName需替换：ABP源码中使用FixedLocalizableString（硬编码英文），无法通过扩展本地化源实现中文，故替换为UtilsLI()。
+        /// 邮件设置项的DisplayName无需替换：ABP源码中使用LocalizableString引用"Abp"本地化源，该源已内置中文翻译，仅需调整Group即可。
+        /// </summary>
+        private void AdjustBuiltInSettingDefinitions()
+        {
+            var settingDefinitionManager = IocManager.Resolve<ISettingDefinitionManager>();
+            var allSettings = settingDefinitionManager.GetAllSettingDefinitions();
 
+            var userMgmt = new SettingDefinitionGroup("UserManagement", "用户管理".UtilsLI());
+            var emailSetting = new SettingDefinitionGroup("EmailSetting", "邮件设置".UtilsLI());
 
-            //运行ZLJ.Migrator时会确实依赖服务，所以这里try下 但是这里try了 若正式用的时候你都不晓得错没错
-            // try
-            //{
+            foreach (var s in allSettings)
+            {
+                switch (s.Name)
+                {
+                    #region 用户管理
+                    case AbpZeroSettingNames.UserManagement.IsEmailConfirmationRequiredForLogin:
+                        s.Group = userMgmt;
+                        s.DisplayName = "登录需要邮箱确认".UtilsLI();
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    case AbpZeroSettingNames.UserManagement.UserLockOut.IsEnabled:
+                        s.Group = userMgmt;
+                        s.DisplayName = "启用账户锁定".UtilsLI();
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    case AbpZeroSettingNames.UserManagement.UserLockOut.MaxFailedAccessAttemptsBeforeLockout:
+                        s.Group = userMgmt;
+                        s.DisplayName = "锁定前最大失败尝试次数".UtilsLI();
+                        s.CustomData = new { csharpType = "int", min = 1, max = 100 };
+                        break;
+                    case AbpZeroSettingNames.UserManagement.UserLockOut.DefaultAccountLockoutSeconds:
+                        s.Group = userMgmt;
+                        s.DisplayName = "默认账户锁定时间(秒)".UtilsLI();
+                        s.CustomData = new { csharpType = "int", min = 1 };
+                        break;
+                    #endregion
 
-            //Task.Yield();
+                    #region 密码策略
+                    case AbpZeroSettingNames.UserManagement.PasswordComplexity.RequiredLength:
+                        s.Group = userMgmt;
+                        s.DisplayName = "密码最小长度".UtilsLI();
+                        s.CustomData = new { csharpType = "int", min = 1, max = 100 };
+                        break;
+                    case AbpZeroSettingNames.UserManagement.PasswordComplexity.RequireNonAlphanumeric:
+                        s.Group = userMgmt;
+                        s.DisplayName = "要求非字母数字字符".UtilsLI();
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    case AbpZeroSettingNames.UserManagement.PasswordComplexity.RequireLowercase:
+                        s.Group = userMgmt;
+                        s.DisplayName = "要求小写字母".UtilsLI();
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    case AbpZeroSettingNames.UserManagement.PasswordComplexity.RequireUppercase:
+                        s.Group = userMgmt;
+                        s.DisplayName = "要求大写字母".UtilsLI();
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    case AbpZeroSettingNames.UserManagement.PasswordComplexity.RequireDigit:
+                        s.Group = userMgmt;
+                        s.DisplayName = "要求数字".UtilsLI();
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    #endregion
 
-            //}
-            // catch
-            // {
+                    #region 双因素认证
+                    case AbpZeroSettingNames.UserManagement.TwoFactorLogin.IsEnabled:
+                        s.Group = userMgmt;
+                        s.DisplayName = "启用双因素登录".UtilsLI();
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    case AbpZeroSettingNames.UserManagement.TwoFactorLogin.IsEmailProviderEnabled:
+                        s.Group = userMgmt;
+                        s.DisplayName = "启用邮箱验证".UtilsLI();
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    case AbpZeroSettingNames.UserManagement.TwoFactorLogin.IsSmsProviderEnabled:
+                        s.Group = userMgmt;
+                        s.DisplayName = "启用短信验证".UtilsLI();
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    case AbpZeroSettingNames.UserManagement.TwoFactorLogin.IsRememberBrowserEnabled:
+                        s.Group = userMgmt;
+                        s.DisplayName = "启用记住浏览器".UtilsLI();
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    #endregion
 
-            //}
+                    #region 邮件设置
+                    case EmailSettingNames.Smtp.Host:
+                        s.Group = emailSetting;
+                        break;
+                    case EmailSettingNames.Smtp.Port:
+                        s.Group = emailSetting;
+                        s.CustomData = new { csharpType = "int", min = 1, max = 65535 };
+                        break;
+                    case EmailSettingNames.Smtp.UserName:
+                        s.Group = emailSetting;
+                        break;
+                    case EmailSettingNames.Smtp.Password:
+                        s.Group = emailSetting;
+                        break;
+                    case EmailSettingNames.Smtp.Domain:
+                        s.Group = emailSetting;
+                        break;
+                    case EmailSettingNames.Smtp.EnableSsl:
+                        s.Group = emailSetting;
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    case EmailSettingNames.Smtp.UseDefaultCredentials:
+                        s.Group = emailSetting;
+                        s.CustomData = new { csharpType = "bool" };
+                        break;
+                    case EmailSettingNames.DefaultFromAddress:
+                        s.Group = emailSetting;
+                        break;
+                    case EmailSettingNames.DefaultFromDisplayName:
+                        s.Group = emailSetting;
+                        break;
+                    #endregion
+                }
+            }
         }
     }
 }
--- src/ZLJ.Admin.CoreRCL/wwwroot/ZLJ.Admin.CoreRCL.lib.module.js
+++ src/ZLJ.Admin.CoreRCL/wwwroot/ZLJ.Admin.CoreRCL.lib.module.js
@@ -1,5 +1,5 @@
 export function onRuntimeConfigLoaded(config) {
     //.net10 bug https://github.com/dotnet/aspnetcore/issues/64009#issuecomment-3534259401
     config.disableNoCacheFetch = true;
-    console.log("onRuntimeConfigLoaded222");
+    console.log("onRuntimeConfigLoaded22");
 }
